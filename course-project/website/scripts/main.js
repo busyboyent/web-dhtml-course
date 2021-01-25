e7aeb6b9f5692eab15ba455cc36e9e6eccf2d916
busyboyent <n79630453145@gmail.com>
2021-01-25 05:49:25 +0500

    error google cloud platform(

---
+++ course-project/website/scripts/main.js
@@ -0,0 +1,34 @@
+/*const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}*/
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/screen.png') {
+      myImage.setAttribute('src','images/kittens.png');
+    } else {
+      myImage.setAttribute('src','images/screen.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
